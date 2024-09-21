a973ef6eb246e7985b884b8394c033486e6d900c
Songhamin01 <stdhamin@gmail.com>
2024-09-21 16:41:39 +0900

    Additional Database

--- Calendar_App/app/src/main/java/com/example/calendar_app/TaskAdapter.kt
+++ Calendar_App/app/src/main/java/com/example/calendar_app/TaskAdapter.kt
@@ -6,20 +6,23 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class TaskAdapter(private val tasks: List<String>) :
+class TaskAdapter(private val tasks: List<Task>) :
     RecyclerView.Adapter<TaskAdapter.TaskViewHolder>() {
 
     class TaskViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val textView: TextView = view.findViewById(android.R.id.text1)
+        val titleTextView: TextView = view.findViewById(R.id.task_title)
+        val contentTextView: TextView = view.findViewById(R.id.task_content)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(android.R.layout.simple_list_item_1, parent, false)
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.task_item, parent, false)
         return TaskViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: TaskViewHolder, position: Int) {
-        holder.textView.text = tasks[position]
+        val task = tasks[position]
+        holder.titleTextView.text = task.title
+        holder.contentTextView.text = task.content
     }
 
     override fun getItemCount() = tasks.size
---
+++ Calendar_App/app/src/main/java/com/example/calendar_app/TaskRepository.kt
@@ -0,0 +1,40 @@
+package com.example.calendar_app
+
+class TaskRepository(private val taskDao: TaskDao) {
+
+    // 모든 Task를 가져오는 메서드
+    suspend fun getAllTasks(): List<Task> {
+        return taskDao.getAllTasks()    // suspend 함수 호출
+    }
+
+    // Task 삽입
+    suspend fun insert(task: Task) {
+        if(task.title.isNotEmpty()) {
+            taskDao.insert(task)
+        }
+    }
+
+    // Task 업데이트
+    suspend fun update(task: Task) {
+        taskDao.update(task)
+    }
+
+    // Task 삭제
+    suspend fun delete(task: Task) {
+        taskDao.delete(task)
+    }
+
+    // 특정 id로 Task 가져오기
+    suspend fun getTaskById(id: Int): Task? {
+        return taskDao.getTaskById(id)
+    }
+
+    suspend fun getTaskByDate(date: String): List<Task?> {
+        return taskDao.getTaskByDate(date)
+    }
+
+    suspend fun deleteAllTasks() {
+        taskDao.deleteAllTasks()
+    }
+
+}
